0f40df7492d26c64835f6d40ce25dab8aa80fa73
noemilaina <s311037@studenti.polito.it>
2025-04-16 13:55:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,11 +17,11 @@ int main()
         return 1;
     }
 	
-	Gedim::UCDUtilities utilities;
-    utilities.ExportPoints("./Cell0Ds.inp",
+Gedim::UCDUtilities utilities;
+utilities.ExportPoints("./Cell0Ds.inp",
                            mesh.Cell0DsCoordinates);
 
-    utilities.ExportSegments("./Cell1Ds.inp",
+utilities.ExportSegments("./Cell1Ds.inp",
                              mesh.Cell0DsCoordinates,
                              mesh.Cell1DsExtrema);
 
