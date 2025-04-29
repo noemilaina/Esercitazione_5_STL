2c30cb9d017d7d3d0de41c4a0c3df716c8c071b3
noemilaina <s311037@studenti.polito.it>
2025-04-29 18:01:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,12 +16,42 @@ int main()
         cerr << "file not found" << endl;
         return 1;
     }
+	map<unsigned int, list<unsigned int>>& m_0Ds_markers = mesh.Cell0Ds_markers;
+	map<unsigned int, list<unsigned int>>& m_1Ds_markers = mesh.Cell01s_markers;
+	map<unsigned int, list<unsigned int>>& m_2Ds_markers = mesh.Cell02s_markers;
 	
-Gedim::UCDUtilities utilities;
-utilities.ExportPoints("./Cell0Ds.inp",
+	for(auto& [marker, list_id] : m_0Ds_markers)
+     {
+        cout<<"Marker0D: "<<marker<<"  IDs = [";
+        for(auto& id : list_id)
+            cout<<' '<<id;
+        cout<<" ]"<<endl;
+     }
+
+     cout<<endl;
+     for(auto& [marker, list_id] : m_1Ds_markers)
+     {
+        cout<<"Marker1D: "<<marker<<"  IDs = [";
+        for(auto& id : list_id)
+            cout<<' '<<id;
+        cout<<" ]"<<endl;
+     }
+
+     cout<<endl;
+     for(auto& [marker, list_id] : m_2Ds_markers)
+     {
+        cout<<"Marker2D: "<<marker<<"  IDs = [";
+        for(auto& id : list_id)
+            cout<<' '<<id;
+        cout<<" ]"<<endl;
+     }
+
+	
+	Gedim::UCDUtilities utilities;
+    utilities.ExportPoints("./Cell0Ds.inp",
                            mesh.Cell0DsCoordinates);
 
-utilities.ExportSegments("./Cell1Ds.inp",
+    utilities.ExportSegments("./Cell1Ds.inp",
                              mesh.Cell0DsCoordinates,
                              mesh.Cell1DsExtrema);
 
